2604659c0083a00665f42811197544ef18c1b000
Carolina Vitória <142808135+carolinavitoria786@users.noreply.github.com>
2024-10-27 19:51:30 -0300

    Add files via upload

--- Pizza/Program.cs
+++ Pizza/Program.cs
@@ -1,19 +1,13 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
-using PizzaStoreApi.Data;
-using PizzaStoreApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(
-    c =>
-    {
-      c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pizza Store API", Description = "Fazendo Pizzas por amor", Version = "v1" });
-    }
-);
 
-var connectionString= builder.Configuration.GetConnectionString("Massas") ?? "Data Source=Pizza.db";
-builder.Services.AddSqlite<AppDbContext>(connectionString);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Fazendo pizzas por amor", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -21,52 +15,9 @@ if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
   app.UseSwaggerUI(
-    c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pizza")
-  );
+    c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API")
+    );
 }
-
-app.UseHttpsRedirection();
-
-app.MapGet("/pizzas", async (AppDbContext db) => await db.Pizzas.ToListAsync());
-
-app.MapGet("/pizza/{id}", async (AppDbContext db, int id) => await db.Pizzas.FindAsync(id));
-
-app.MapPut("/pizza/{id}", async (AppDbContext
-db, Pizza updatepizza, int id) =>
-{
-  var pizza = await db.Pizzas.FindAsync(id);
-  if (pizza is null) return
-  Results.NotFound();
-  pizza.Nome = updatepizza.Nome;
-  pizza.Tamanho = updatepizza.Tamanho;
-  pizza.Preco = updatepizza.Preco;
-  await db.SaveChangesAsync();
-  return Results.NoContent();
-});
-
-app.MapDelete("/pizza/{id}", async
-(AppDbContext db, int id) =>
-{
-  var pizza = await db.Pizzas.FindAsync(id);
-  if (pizza is null)
-  {
-    return Results.NotFound();
-  }
-  db.Pizzas.Remove(pizza);
-  await db.SaveChangesAsync();
-  return Results.Ok();
-});
-
-app.MapPost("/pizza", async
-(AppDbContext db, Pizza pizza) =>
-{
-
-  await db.Pizzas.AddAsync(pizza);
-  await db.SaveChangesAsync();
-  return
-
-  Results.Created($"/pizza/{pizza.Id}",
-  pizza);
-});
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
